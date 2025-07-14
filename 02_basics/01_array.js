72e613412477d22fb79c526774aa84d436763561
12-Amandeep <amandeepvaid12@gmail.com>
2025-07-14 10:32:57 +0000

    Part-2(DSA basics)

---
+++ 02_basics/01_array.js
@@ -0,0 +1,17 @@
+// Arrays
+
+const myarr = [0, 1, 2, 3, 4, 5]
+// console.log(myarr[3]);
+
+//Methods
+// myarr.push(6);
+// myarr.unshift(9);
+// myarr.shift();
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(9));
+// const newarr = myarr.join();
+// console.log(typeof newarr);
+
+//+++++++Slice and Splice++++++
+
+console.log(myarr);
