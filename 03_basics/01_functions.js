da89d35cfcab9a353308acb9033a559b5c05cbb8
12-Amandeep <amandeepvaid12@gmail.com>
2025-07-17 13:07:36 +0000

    Functions started

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,21 @@
+function sayMyName(){
+    console.log("Reenu");
+    console.log("Aman");
+}
+
+// sayMyName()
+
+function addTwoNum(num1, num2){
+    let res = num1 + num2
+
+    return res
+}
+
+const result = addTwoNum(3, 6)
+// console.log(result);
+
+function loginUser(username){
+    return `${username} just logged in.`
+}
+
+console.log(loginUser("Aman"));
